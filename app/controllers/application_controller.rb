818a6fd072373b3dc4c6f032a1a91361ee17e1f8
pintookewat <pintookewat88@gmail.com>
2022-11-20 13:07:08 +0530

    [solve devise error]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :phone, :gender, :proffesion, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit([ :full_name, :phone, :gender, :proffesion, :email, :password, :password_confirmation, :current_password])
+    end
   end
-
 end
