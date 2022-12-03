d82f9d1297c9227c6c3432530a5f16099b5fefc8
pintookewat <pintookewat88@gmail.com>
2022-12-03 17:22:08 +0530

    [change database]

--- db/migrate/20221116182641_adddetail_touser.rb
+++ db/migrate/20221116182641_adddetail_touser.rb
@@ -2,7 +2,7 @@ class AdddetailTouser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :full_name, :string
     add_column :users, :gender, :string 
-    add_column :users, :phone, :integer
+    add_column :users, :phone, :string
     add_column :users, :proffesion, :string        
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_16_182641) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -33,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_182641) do
     t.datetime "updated_at", null: false
     t.string "full_name"
     t.string "gender"
-    t.integer "phone"
+    t.string "phone"
     t.string "proffesion"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
