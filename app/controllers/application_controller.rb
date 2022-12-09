2961a4b598f6cd569244b562de269725fb75085f
pintookewat <pintookewat88@gmail.com>
2022-12-10 02:07:15 +0530

    [show database ]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+ 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit([ :image, :image_cache, :full_name, :phone, :gender, :proffesion, :email, :password, :password_confirmation, :current_password])
+    user_params.permit([ :image, :image_cache, :full_name, :phone, :gender, :proffesion, :email, :password, :password_confirmation, :current_password])
+    end
+    devise_parameter_sanitizer.permit(:edit) do |user_params|
+    user_params.permit([ :image, :image_cache, :full_name, :phone, :gender, :proffesion, :email, :password, :password_confirmation, :current_password])
     end
   end
   
+
+
   def after_sign_in_path_for(resource)
     user_user_index_path 
   end
