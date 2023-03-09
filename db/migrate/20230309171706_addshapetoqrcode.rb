6d837fe0b80190d6450b18754a1c8d84ac45183b
Pintu kewat <pintookewat88@gmail.com>
2023-03-10 00:25:00 +0530

    [add shapes]

---
+++ db/migrate/20230309171706_addshapetoqrcode.rb
@@ -0,0 +1,5 @@
+class Addshapetoqrcode < ActiveRecord::Migration[7.0]
+  def change
+     add_column :qrcodes, :shape, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_124244) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_171706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text "content"
@@ -53,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_124244) do
     t.datetime "deleted_at"
     t.string "slug"
     t.string "checkbox_list"
+    t.string "shape"
   end
 
   create_table "users", force: :cascade do |t|
